865833feaff0a37f6fe2d3b6f6ade8a0d51b6cf0
BenJensen2 <btjensen@mtu.edu>
2020-11-21 19:47:47 -0500

    OOP

---
+++ Object_Oriented_Programming/Class.js
@@ -0,0 +1,5 @@
+// There are no classes in JavaScript.
+// It is a prototype based object oriented language,
+// which means it doesn't have classes rather
+// it defines behaviors using a constructor function
+// and then reuse it using the prototype.
